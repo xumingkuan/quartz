72b912e91d387a04fc57e3f3e361ff49cc96ed46
xumingkuan <xumingkuan0721@126.com>
2023-04-17 21:48:51 -0400

    Include the number of swaps

--- scripts/ilp_plot.py
+++ scripts/ilp_plot.py
@@ -2,7 +2,7 @@ import math
 
 import matplotlib.pyplot as plt
 
-results = [{}, {}]  # heuristics, ilp
+results = [{}, {}, {}, {}]  # heuristics (#stages, #swaps), ilp (#stages, #swaps)
 num_local_qubits_start = 23
 num_of_num_local_qubits = {}
 circuit_name = None
@@ -14,7 +14,7 @@ with open('../ilp_result.csv') as f:
             circuit_name = line
             continue
         num_qubits = int(numbers[0].strip())
-        for i in range(2):
+        for i in range(4):
             if num_qubits not in results[i].keys():
                 results[i][num_qubits] = {}
         if num_qubits not in num_of_num_local_qubits.keys():
@@ -23,14 +23,12 @@ with open('../ilp_result.csv') as f:
             # not finished, discard this line
             continue
         # heuristic results come first
-        if circuit_name not in results[0][num_qubits].keys():
-            results[0][num_qubits][circuit_name] = [
-                int(num.strip()) for num in numbers[1:]
-            ]
-        elif circuit_name not in results[1][num_qubits].keys():
-            results[1][num_qubits][circuit_name] = [
-                int(num.strip()) for num in numbers[1:]
-            ]
+        for j in range(4):
+            if circuit_name not in results[j][num_qubits].keys():
+                results[j][num_qubits][circuit_name] = [
+                    int(num.strip()) for num in numbers[1:]
+                ]
+                break
         else:
             assert False
 
@@ -50,16 +48,16 @@ labels = ['Torque', 'SnuQS']
 for num_qubits in num_of_num_local_qubits.keys():
     plt.cla()
     plt.figure(constrained_layout=True)
-    for i in range(2):
+    for i in [2, 0]:
         plt.plot(
             range(
                 num_local_qubits_start,
                 num_local_qubits_start + num_of_num_local_qubits[num_qubits],
             ),
-            results_geomean[1 - i][num_qubits],
-            ['x-', '.--'][i],
+            results_geomean[i][num_qubits],
+            ['x-', '.--'][0 if i == 2 else 1],
             markersize=8,
-            label=labels[i],
+            label=labels[0 if i == 2 else 1],
         )
     plt.xlabel('Number of local qubits', fontsize=12, fontweight='bold')
     plt.xticks(
@@ -77,15 +75,57 @@ for num_qubits in num_of_num_local_qubits.keys():
     legend = plt.legend(fontsize=12, ncol=2, loc='lower left')
     # text = legend.get_texts()[0]
     # text.set_fontfamily('Sans Serif')
-    fig.set_size_inches(5.9, 2.7)
+    fig.set_size_inches(5.6, 2.7)
     fig.savefig(f'ilp_plot_{num_qubits}.pdf', dpi=800)
+    print(
+        f'''
+\\begin{{minipage}}[2.3in]
+\\includegraphics[width=0.99\linewidth]{{figures/ilp_plot_{num_qubits}.pdf}}
+\\caption{{The geometric mean of the number of stages among six circuits in MQT Bench with {num_qubits} qubits.}}
+\\label{{fig:ilp_{num_qubits}}}
+\\end{{minipage}}
+'''
+    )
+    plt.close()
+
+    plt.cla()
+    plt.figure(constrained_layout=True)
+    for i in [3, 1]:
+        plt.plot(
+            range(
+                num_local_qubits_start,
+                num_local_qubits_start + num_of_num_local_qubits[num_qubits],
+            ),
+            results_geomean[i][num_qubits],
+            ['x-', '.--'][0 if i == 3 else 1],
+            markersize=8,
+            label=labels[0 if i == 3 else 1],
+        )
+    plt.xlabel('Number of local qubits', fontsize=12, fontweight='bold')
+    plt.xticks(
+        range(
+            num_local_qubits_start,
+            num_local_qubits_start + num_of_num_local_qubits[num_qubits],
+            1 if num_qubits == 31 else 2,
+        ),
+        fontsize=12,
+    )
+    plt.ylabel('Geomean of #swaps', fontsize=12, fontweight='bold')
+    plt.yticks(fontsize=12)
+    plt.ylim(bottom=0)
+    fig = plt.gcf()
+    legend = plt.legend(fontsize=12, ncol=2, loc='lower left')
+    # text = legend.get_texts()[0]
+    # text.set_fontfamily('Sans Serif')
+    fig.set_size_inches(5.6, 2.7)
+    fig.savefig(f'ilp_swaps_{num_qubits}.pdf', dpi=800)
     print(
         f'''
 \\begin{{figure}}[t]
 \\centering
-\\includegraphics[width=0.99\linewidth]{{figures/ilp_plot_{num_qubits}.pdf}}
-\\caption{{The geometric mean of the number of stages among 12 circuits in MQT Bench with {num_qubits} qubits.}}
-\\label{{fig:ilp_{num_qubits}}}
+\\includegraphics[width=0.99\linewidth]{{figures/ilp_swaps_{num_qubits}.pdf}}
+\\caption{{The geometric mean of the number of swaps among six circuits in MQT Bench with {num_qubits} qubits.}}
+\\label{{fig:ilp_swaps_{num_qubits}}}
 \\end{{figure}}
 '''
     )
--- src/benchmark/ilp_num_stages.cpp
+++ src/benchmark/ilp_num_stages.cpp
@@ -14,7 +14,8 @@
 using namespace quartz;
 
 int num_stages_by_heuristics(CircuitSeq *seq, int num_local_qubits,
-                             std::vector<std::vector<bool>> &local_qubits) {
+                             std::vector<std::vector<bool>> &local_qubits,
+                             int &num_swaps) {
   int num_qubits = seq->get_num_qubits();
   std::unordered_map<CircuitGate *, bool> executed;
   // No initial configuration -- all qubits are global.
@@ -128,6 +129,13 @@ int num_stages_by_heuristics(CircuitSeq *seq, int num_local_qubits,
     }
     std::cout << "}" << std::endl;
     local_qubits.push_back(local_qubit);
+    if (num_stages != 1) {
+      for (int i = 0; i < num_local_qubits; i++) {
+        if (!local_qubits[num_stages - 2][candidate_indices[i]]) {
+          num_swaps++;
+        }
+      }
+    }
   }
   std::cout << num_stages << " stages." << std::endl;
   return num_stages;
@@ -140,12 +148,8 @@ int main() {
   Context ctx({GateType::input_qubit, GateType::input_param, GateType::h,
                GateType::x, GateType::ry, GateType::u2, GateType::u3,
                GateType::cx, GateType::cz, GateType::cp, GateType::swap});
-  std::vector<std::string> circuit_names = {"ae",
-                                            "ghz",
-                                            "graphstate",
-                                            "qft",
-                                            "qpeexact",
-                                            "su2random"};
+  std::vector<std::string> circuit_names = {"ae",  "ghz",       "graphstate",
+                                            "qft", "qpeexactm", "su2random"};
   // 31 or 42 total qubits, 23-42 local qubits
   std::vector<int> num_qubits = {31, 42};
   std::vector<int> num_local_qubits;
@@ -162,17 +166,26 @@ int main() {
                     ".qasm");
 
       fprintf(fout, "%d, ", num_q);
+      std::vector<int> n_swaps;
       for (int local_q : num_local_qubits) {
         if (local_q > num_q) {
           continue;
         }
         std::vector<std::vector<bool>> local_qubits_by_heuristics;
+        int num_swaps = 0;
         int heuristics_result = num_stages_by_heuristics(
-            seq.get(), local_q, local_qubits_by_heuristics);
+            seq.get(), local_q, local_qubits_by_heuristics, num_swaps);
+        n_swaps.push_back(num_swaps);
         fprintf(fout, "%d, ", heuristics_result);
         fflush(fout);
       }
       fprintf(fout, "\n");
+      fprintf(fout, "%d, ", num_q);
+      for (auto &num_swaps : n_swaps) {
+        fprintf(fout, "%d, ", num_swaps);
+      }
+      fprintf(fout, "\n");
+      fflush(fout);
     }
     for (int num_q : num_qubits) {
       auto seq = CircuitSeq::from_qasm_file(
@@ -182,6 +195,7 @@ int main() {
 
       fprintf(fout, "%d, ", num_q);
       int answer_start_with = 1;
+      std::vector<int> n_swaps;
       for (int local_q : num_local_qubits) {
         if (local_q > num_q) {
           continue;
@@ -190,19 +204,37 @@ int main() {
         local_qubits = compute_local_qubits_with_ilp(
             *seq, local_q, &ctx, &interpreter, answer_start_with);
         int ilp_result = (int)local_qubits.size();
+        int num_swaps = 0;
+        std::vector<bool> prev_local(num_q, false);
         for (int j = 0; j < ilp_result; j++) {
           std::cout << "Stage " << j << ": ";
           std::cout << local_qubits.size() << " local qubits, ";
           for (int k : local_qubits[j]) {
             std::cout << k << " ";
+            if (j > 0) {
+              if (!prev_local[k]) {
+                num_swaps++;
+              }
+            }
+          }
+          prev_local.assign(num_q, false);
+          for (int k : local_qubits[j]) {
+            prev_local[k] = true;
           }
           std::cout << std::endl;
         }
+        n_swaps.push_back(num_swaps);
         fprintf(fout, "%d, ", ilp_result);
         fflush(fout);
         answer_start_with = ilp_result;
       }
       fprintf(fout, "\n");
+      fprintf(fout, "%d, ", num_q);
+      for (auto &num_swaps : n_swaps) {
+        fprintf(fout, "%d, ", num_swaps);
+      }
+      fprintf(fout, "\n");
+      fflush(fout);
     }
   }
   fclose(fout);
